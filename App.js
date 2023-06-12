830fbb96edbad9e3bf1b9784ec551ee657f4360e
Antriksh Gupta <Antrikshgupta01@gmail.com>
2023-06-12 21:23:11 +0530

    Fonts added

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import {StatusBar} from 'react-native';
+import React, { useEffect }from 'react';
+import {StatusBar, StyleSheet } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Drawer from './src/screens/drawer';
 import Main_home from './src/screens/Homepage/Main_home';
+import { font } from './src/constants/font';
 
 const App = () => {
+
+  //Fonts
+  font();
 
   return (
     <SafeAreaProvider style={{ flex: 1 }}>
